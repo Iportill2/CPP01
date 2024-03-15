5618d6ccd1cc8605eea4c21564b014bce67eccf0
Iportill2 <iportill@student.42urduliz.com>
2024-03-15 13:49:15 +0100

    con this comentado

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -3,17 +3,17 @@
 Zombie::Zombie(std::string name)
 {
 	_name = name;
-	std::cout << this->_name << " zombie has been created" << std::endl;
+	std::cout << /* this-> */_name << " zombie has been created" << std::endl;
 }
 
 Zombie::~Zombie()
 {
-	std::cout << "zombie named " << this->_name << " Died"<< std::endl;
+	std::cout << "zombie named " << /* this-> */_name << " Died"<< std::endl;
 }
 
 
 void Zombie::announce()
 {
-	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << /* this-> */_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
