079ab5dd68756360113dc6fbdae82c4a2df4dc99
Iportill2 <iportill@student.42urduliz.com>
2024-03-20 12:06:53 +0100

    cpp01 terminado

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -2,10 +2,6 @@
 Harl::Harl()
 {
 	std::cout << "Constructor!" << std::endl;
-	_debug="DEBUG";
-	_info="INFO";
-	_warning="WARNING";
-	_error="ERROR";
 }
 Harl::~Harl()
 {
@@ -13,39 +9,37 @@ Harl::~Harl()
 }
 void Harl::complain( std::string level )
 {
-	if (_debug == level)
-		debug();
-	else if(level == _info)
-		info();
-	else if(level == _warning)
-		warning();
-	else if(level == _error)
-		error();
-	else
-		endl_print("Invalid command, please try again");
-	
-}
-void Harl::print(std::string s)
-{
-	std::cout << s;
+	std::string	lvl[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void		(Harl::*ptrComplain[4])(void) = {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
+	int i =0;
+	while (i < 4)
+	{
+		if (level == lvl[i])
+		{
+			(this->*ptrComplain[i])();
+			return ;
+		}
+		i++;
+	}
+	std::cout << "Unexpected input" << std::endl;
 }
 void Harl::endl_print(std::string s)
 {
 	std::cout << s << std::endl;
 }
-void Harl::debug()
+void Harl::_debug()
 {
-	endl_print("I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!") ;
+	endl_print("Debug messages contain contextual information. They are mostly used for problem diagnosis") ;
 }
-void Harl::info()
+void Harl::_info()
 {
-	endl_print(" cannot believe adding extra bacon costs more money. You didn´t put enough bacon in my burger! If you did, I wouldn’t be asking for more!");
+	endl_print("Info messages contain extensive information. They are helpful for tracing program execution in a production environment.");
 }
-void Harl::warning()
+void Harl::_warning()
 {
-	endl_print(" think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.");
+	endl_print("Warning messages indicate a potential issue in the system. However, it can be handled or ignored.");
 }
-void Harl::error()
+void Harl::_error()
 {
-	endl_print("This is unacceptable! I want to speak to the manager now.");
+	endl_print("Error messages indicate an unrecoverable error has occurred. This is usually a critical issue that requires manual intervention.");
 }
--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -5,21 +5,16 @@ class Harl
 {
 	private:
 
-	std::string _debug;
-	std::string _info;
-	std::string _warning;
-	std::string _error;
-	void		debug( void );
-	void		info( void );
-	void		warning( void );
-	void		error( void );
+	void		_debug( void );
+	void		_info( void );
+	void		_warning( void );
+	void		_error( void );
 
 	public:
 
 				Harl();
 				~Harl();
 	void 		complain( std::string level );
-	void		print(std::string s);
 	void		endl_print(std::string s);
 };
 #endif
